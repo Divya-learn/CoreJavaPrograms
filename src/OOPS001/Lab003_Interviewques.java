513ae51ea78e3ce962a27dab58c4f2c20c4c977c
Divya <divyapreethyr@gmail.com>
2024-01-16 20:31:22 +0530

    commit

---
+++ src/OOPS001/Lab003_Interviewques.java
@@ -0,0 +1,24 @@
+package src.OOPS001;
+
+public class Lab003_Interviewques {
+
+    public static void main(String[] args) {
+        System.out.println(args[0]);    //Passing the argument in edit configuration
+
+
+        //In before we will pass the arguments in edit configuration
+
+        //But now we will use Scanner class
+        main(10);
+    }
+
+    public static void main(int a) //we can have same method name but different arguments
+    {
+        System.out.println("void o/p is" + a);
+    }
+
+    public static int main(int a, int b)
+    {
+        return 99;
+    }
+}
