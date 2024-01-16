e14fa8b0cdf26a666f1bbba1114accd35d527001
Divya <divyapreethyr@gmail.com>
2024-01-16 12:17:33 +0530

    commit

---
+++ src/OOPS001/Lab001Methodsfunctions.java
@@ -0,0 +1,38 @@
+package src.OOPS001;
+
+public class Lab001Methodsfunctions {
+
+    public static void main(String[] args)
+    {
+     int c=  sum(3,5);                 //function call    3 and 5 are arguments
+     System.out.println(c); //Two arguments
+
+     int c1 = sum(4);   //One arguments
+        System.out.println(c1);
+
+     int c3 = sum();        //No arguments
+     System.out.println(c3);
+
+     sayhello();
+    }
+
+    static int sum(int a, int b)            //function declaration
+    {
+        return a+b;                       //function definition
+    }
+
+    static int sum(int a)
+    {
+        return a;
+    }
+
+    static int sum()
+    {
+        return 99;
+    }
+
+    static void sayhello()
+    {
+        System.out.println("Yes");
+    }
+}
