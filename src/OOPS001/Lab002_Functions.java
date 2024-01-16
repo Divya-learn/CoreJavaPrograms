df0975573a5a5d4208c5837554e4a9b22a8aa3ed
Divya <divyapreethyr@gmail.com>
2024-01-16 20:02:46 +0530

    commit

---
+++ src/OOPS001/Lab002_Functions.java
@@ -0,0 +1,16 @@
+package src.OOPS001;
+
+public class Lab002_Functions {
+
+    public static void main(String[] args){
+
+       sayhello();
+       sayhello();
+    }
+
+    static void sayhello()
+    {
+        int a =10, b=30;
+        System.out.println(a+b);
+    }
+}
