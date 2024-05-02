eeb9f2b968fc1badc2d8fefefe4876026c59dc28
Divya <divyapreethyr@gmail.com>
2024-05-02 14:01:10 +0530

    commit

---
+++ src_interviewQuestion/P3_Printhello_worldwithoutusing_semicolon.java
@@ -0,0 +1,17 @@
+package src_interviewQuestion;
+
+public class P3_Printhello_worldwithoutusing_semicolon {
+
+    //print helloworld withoutusing semicolon
+    public static void main(String[] args){
+
+        for(int i=0;i<1;System.out.println("Hello world")){
+            i++;
+        }
+        //or
+
+        if(System.out.printf("Hello world")==null){
+
+        }
+    }
+}
